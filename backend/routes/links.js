c9939be7a62e69ca962fb3a1f8aa747954843a31
efrat <efrat.jamil@gmail.com>
2026-04-13 17:53:45 +0300

    add

--- backend/routes/links.js
+++ backend/routes/links.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { postScanLink } = require("../controllers/linkController");
 
-// POST /api/links/scan
 router.post("/scan", postScanLink);
 
 module.exports = router;
